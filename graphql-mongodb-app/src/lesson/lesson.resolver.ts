d67906e4edf51b3c4aa35c0dcb21fbe2113931dc
Conary <31629328+conaryHernandez@users.noreply.github.com>
2020-04-12 09:10:56 -0600

    creating and getting lessons

--- graphql-mongodb-app/src/lesson/lesson.resolver.ts
+++ graphql-mongodb-app/src/lesson/lesson.resolver.ts
@@ -1,15 +1,21 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { LessonType } from './lesson.type';
+import { LessonService } from './lesson.service';
+import { CreateLessonInput } from './lesson.input';
 
 @Resolver(of => LessonType)
 export class LessonResolver {
+  constructor(private lessonService: LessonService) {}
+
   @Query(returns => LessonType)
-  lesson() {
-    return {
-      id: '123',
-      name: 'Physics Class',
-      startDate: new Date().toISOString(),
-      endDate: new Date().toISOString(),
-    };
+  lesson(@Args('id') id: string) {
+    return this.lessonService.getLesson(id);
+  }
+
+  @Mutation(returns => LessonType)
+  createLesson(
+    @Args('createLessonInput') createLessonInput: CreateLessonInput,
+  ) {
+    return this.lessonService.createLesson(createLessonInput);
   }
 }
