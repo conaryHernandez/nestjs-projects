d59d4468f1ea3b32e85a9162bc7388bf7c154fb8
Conary <31629328+conaryHernandez@users.noreply.github.com>
2020-04-13 14:57:10 -0600

    query get students

--- graphql-mongodb-app/src/student/student.resolver.ts
+++ graphql-mongodb-app/src/student/student.resolver.ts
@@ -1,11 +1,21 @@
-import { Resolver, Mutation, Args } from '@nestjs/graphql';
+import { Resolver, Mutation, Args, Query } from '@nestjs/graphql';
 import { StudentType } from './student.type';
 import { CreateStudentInput } from './create-student.input';
 import { StudentService } from './student.service';
 
-@Resolver(StudentType)
+@Resolver(of => StudentType)
 export class StudentResolver {
   constructor(private studentService: StudentService) {}
+
+  @Query(returns => [StudentType])
+  async students() {
+    return this.studentService.getStudents();
+  }
+
+  @Query(returns => StudentType)
+  async student(@Args('id') id: string) {
+    return this.studentService.getStudent(id);
+  }
 
   @Mutation(returns => StudentType)
   async createStudent(
